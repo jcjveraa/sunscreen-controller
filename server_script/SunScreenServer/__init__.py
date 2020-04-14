d5e0a033e95ef37612453526e493895e265de604
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-04-14 21:28:21 +0200

    test travis

--- server_script/SunScreenServer/__init__.py
+++ server_script/SunScreenServer/__init__.py
@@ -1,8 +1,1 @@
 from .version import __version__
-from .OpenWeatherManager import *
-
-# if somebody does "from somepackage import *", this is what they will
-# be able to access:
-__all__ = [
-    'OpenWeatherManager'
-]
--- server_script/SunScreenServer/tests/OpenWeatherManager_test.py
+++ server_script/SunScreenServer/tests/OpenWeatherManager_test.py
@@ -1,25 +1,19 @@
 import unittest
-
-
-
-
+from SunScreenServer import OpenWeatherManager
 class OWMTest(unittest.TestCase):
 
     def test_Kelvin_to_Celcius(self):
-        OpenWeatherManager
-        owm = OpenWeatherManager()
-        self.assertEqual(owm.kelvin_to_celcius(0), -273.15)
-        self.assertNotEqual(owm.kelvin_to_celcius(1), -270.15)
+        self.assertEqual(OpenWeatherManager.kelvin_to_celcius(0), -273.15)
+        self.assertNotEqual(OpenWeatherManager.kelvin_to_celcius(1), -270.15)
 
     def test_is_current_check(self):
-        owm = OpenWeatherManager()
-        owm.get_Open_Weather_JSON()
-        self.assertTrue(owm.oncecall_is_current())
-        self.assertFalse(owm.oncecall_is_current(-(5*60)))
+        json = OpenWeatherManager.get_Open_Weather_JSON()
+        self.assertTrue(OpenWeatherManager.is_current(json))
+        self.assertFalse(OpenWeatherManager.is_current(json, -(5*60)))
 
     def test_next_forecast(self):
-        owm = OpenWeatherManager()
-        self.assertTrue('dt' in owm.get_next_forecast())
+        json = OpenWeatherManager.get_Open_Weather_JSON()
+        self.assertTrue('dt' in OpenWeatherManager.get_next_forecast(json))
 
 if __name__ == '__main__':
     unittest.main()
