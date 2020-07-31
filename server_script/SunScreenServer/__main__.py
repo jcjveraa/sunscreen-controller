050263f92a242392111d377ef4d29ea705ee2d8a
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-07-31 14:38:18 +0200

    Update __main__.py

--- server_script/SunScreenServer/__main__.py
+++ server_script/SunScreenServer/__main__.py
@@ -73,7 +73,7 @@ def main():
 
         if(all(checks)):
             percent_open = OpenPosCalc.get_open_percentage_required(
-                solar_noon, adjustment=5)
+                solar_noon, adjustment=3)
             print("All checks OK!")
             move_sunscreen(percent_open)
         else:
