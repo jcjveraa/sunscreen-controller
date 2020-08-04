6519386aafffafd2bdd9cdc5f49682939ecb1bb1
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-08-04 19:03:02 +0200

    Fix that it should not send out a signal

--- server_script/SunScreenServer/ScreenMover.py
+++ server_script/SunScreenServer/ScreenMover.py
@@ -23,10 +23,9 @@ def move_sunscreen(percent_open: int):
     secrets = get_secrets()
     operate_url = "http://{}/Operate?targetPercentageOpen={}&key={}&timestamp={}"
 
-    # Disable this as we need to keep communicating
-    # if get_current_position() is percent_open:
-      #  print('not moving, current percentage already matches')
-       # exit()
+    if get_current_position() is percent_open:
+       print('not moving, current percentage already matches')
+       exit()
 
     print("moving to " + str(percent_open))
 
